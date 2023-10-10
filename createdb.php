d33498f65b63bee364a8497a16ca0e9c63dd43bc
haileabZ <138156036+haileabZ@users.noreply.github.com>
2023-10-10 17:34:52 +0300

    Create createdb.php

---
+++ createdb.php
@@ -0,0 +1,23 @@
+
+
+
+<?php
+
+include "connect.php";
+
+
+$sql="CREATE DATABASE projectdb";
+
+if(!mysqli_query($con,$sql)){
+
+     echo "datbase not created";
+
+}
+else{
+     echo "you have created a database";
+}
+
+
+
+
+?>
